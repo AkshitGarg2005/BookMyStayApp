b9b7e5d7dfdeeec14e3987daa343cabba414a213
AkshitGarg2005 <akshitgargm8n@gmail.com>
2026-03-18 09:38:13 +0530

    Initial commit for Book My Stay App

---
+++ app/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,9 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("   Welcome to Book My Stay App v1.0");
+        System.out.println("****************************************");
+        System.out.println("Your trusted Hotel Booking Management System");
+        System.out.println("Application started successfully...");
+    }
+}
