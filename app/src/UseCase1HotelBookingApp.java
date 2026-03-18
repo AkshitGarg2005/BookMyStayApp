8c436b760629f2383a0ab0f91ba9d03b84ac60c8
AkshitGarg2005 <akshitgargm8n@gmail.com>
2026-03-18 09:43:13 +0530

    Application Entry & Welcome Message

--- app/src/UseCase1HotelBookingApp.java
+++ app/src/UseCase1HotelBookingApp.java
@@ -1,9 +1,4 @@
+package PACKAGE_NAME;
+
 public class UseCase1HotelBookingApp {
-    public static void main(String[] args) {
-        System.out.println("****************************************");
-        System.out.println("   Welcome to Book My Stay App v1.0");
-        System.out.println("****************************************");
-        System.out.println("Your trusted Hotel Booking Management System");
-        System.out.println("Application started successfully...");
-    }
 }
